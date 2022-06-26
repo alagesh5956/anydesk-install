742a697d100625a10367db571904675537d2182d
alagesh5956 <56873674+alagesh5956@users.noreply.github.com>
2022-06-26 20:15:46 +0530

    Update anydesk.sh

--- anydesk.sh
+++ anydesk.sh
@@ -2,5 +2,3 @@ wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
 echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
 apt update
 apt install anydesk
-exit
-anydesk
